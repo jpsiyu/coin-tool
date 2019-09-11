ccaf42b85cb7edfa21f3c6231d7c6e02acb54a2c
wusiyu <stephenwu2019@163.com>
2019-09-11 15:16:56 +0800

    add project

---
+++ components/Navigator.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="nav">
+    <div class="nav-title">
+      <span>币管理工具</span>
+    </div>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      background-color="#001529"
+      text-color="#fff"
+      active-text-color="#fff"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span>Navigator Three</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  background-color: #001529;
+  width: 256px;
+}
+
+.nav-title {
+  background-color: #002847;
+  color: #fff;
+  font-weight: 700;
+  font-size: 24px;
+  text-align: center;
+  line-height: 68px;
+  height: 68px;
+}
+
+.nav .el-menu{
+  border-right: 0;
+}
+
+.nav >>> .el-menu-item.is-active {
+  background-color: #409eff !important;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="de">
+    <Navigator />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navigator from '@/components/Navigator'
+export default {
+  components: { Navigator }
+}
+</script>
+
+<style>
+
+.de{
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+}
+</style>
